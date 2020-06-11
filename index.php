a3e115a2c1dbe46b0821a0c5b31d52e2690f85be
vorokai <ihor.stepakov@gmail.com>
2020-06-12 02:21:38 +0300

    login

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body style="background-color:#7f8c8d">
+        <div id="main-wrapper">
+            <h2>Login</h2>
+        </div>
+        <form class="myform" action="index.php" method="post">
+            <label>Username</label><br>
+            <input type="text" class="inputvalues" placeholder="Username"><br>
+            <label>Password</label><br>
+            <input type="password" class="inputvalues" placeholder="Username">
+            <button type="submit" id="login_btn" value="Login"/>
+
+        </form>
+    </body>
+</html>
