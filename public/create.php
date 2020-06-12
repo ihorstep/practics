b11c105ae326ad1740cc3b7ed1f907947b5c83bb
vorokai <ihor.stepakov@gmail.com>
2020-06-12 03:00:11 +0300

    db

--- public/create.php
+++ public/create.php
@@ -1,47 +1,51 @@
 <?php
-    if (isset($_POST['submit'])) {
-        require "../config.php";
 
-        $new_user = array(
-            "name" => $_POST['firstname'],
-            "age"     => $_POST['email'],
-            "phone"       => $_POST['age']
+if (isset($_POST['submit'])) {
+    require "../config.php";
+    require "../common.php";
+
+    $new_user = array(
+        "name" => $_POST['firstname'],
+        "age" => $_POST['email'],
+        "phone" => $_POST['age']
+    );
+
+    try {
+        $connection = new PDO($dsn, $username, $password, $options);
+        $sql = sprintf(
+            "INSERT INTO %s (%s) values (%s)",
+            "clients",
+            implode(", ", array_keys($new_user)),
+            ":" . implode(", :", array_keys($new_user))
         );
+        $statement = $connection->prepare($sql);
+        $statement->execute($new_user);
 
-        try {
-            $connection = new PDO($dsn, $username, $password, $options);
-            $sql = sprintf(
-                "INSERT INTO %s (%s) values (%s)",
-                "clients",
-                implode(", ", array_keys($new_user)),
-                ":" . implode(", :", array_keys($new_user))
-            );
-            $statement = $connection->prepare($sql);
-            $statement->execute($new_user);
-
-        } catch(PDOException $error) {
-            echo $sql . "<br>" . $error->getMessage();
-        }
+    } catch (PDOException $error) {
+        echo $sql . "<br>" . $error->getMessage();
     }
+}
 ?>
 
 <?php include "templates/header.php"; ?>
+
+<?php if (isset($_POST['submit']) && $statement) { ?>
+    <?php echo escape($_POST['firstname']); ?> successfully added.
+<?php } ?>
+
 <h2>Add a user</h2>
 
 <form method="post">
-    <label for="firstname">First Name</label>
-    <input type="text" name="firstname" id="firstname">
-    <label for="lastname">Last Name</label>
-    <input type="text" name="lastname" id="lastname">
-    <label for="email">Email Address</label>
-    <input type="text" name="email" id="email">
+    <label for="name">Last Name</label>
+    <input type="text" name="name" id="name">
     <label for="age">Age</label>
     <input type="text" name="age" id="age">
-    <label for="location">Location</label>
-    <input type="text" name="location" id="location">
+    <label for="phone">Phone</label>
+    <input type="text" name="phone" id="phone">
     <input type="submit" name="submit" value="Submit">
 </form>
 
 <a href="index.php">Back to home</a>
+
 
 <?php include "templates/footer.php"; ?>
--- public/read.php
+++ public/read.php
@@ -1,4 +1,49 @@
+<?php
+if (isset($_POST['submit'])) {
+    try {
+        require "../config.php";
+        require "../common.php";
+
+        $connection = new PDO($dsn, $username, $password, $options);
+        $sql = "SELECT *  FROM clients;";
+        $statement = $connection->prepare($sql);
+        $statement->execute();
+        $result = $statement->fetchAll();
+    } catch(PDOException $error) {
+        echo $sql . "<br>" . $error->getMessage();
+    }
+}
+?>
+
 <?php include "templates/header.php"; ?>
+<?php if (isset($_POST['submit'])) {
+    if ($result && $statement->rowCount() > 0) { ?>
+    <h2>Results</h2>
+
+    <table>
+        <thead>
+        <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Phone</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($result as $row) { ?>
+            <tr>
+                <td><?php echo escape($row["id"]); ?></td>
+                <td><?php echo escape($row["name"]); ?></td>
+                <td><?php echo escape($row["age"]); ?></td>
+                <td><?php echo escape($row["phone"]); ?> </td>
+            </tr>
+        <?php } ?>
+        </tbody>
+    </table>
+<?php } else { ?>
+    > No results found
+<?php }
+} ?>
     <h2>Find user based on location</h2>
 
     <form method="post">
